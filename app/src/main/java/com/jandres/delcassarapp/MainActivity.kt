4fabd0ec4f5b122bffa60731668d341b9c9ef367
jandresm618 <jandres.mosquera@udea.edu.co>
2021-04-12 17:57:35 -0500

    MainBinding Habilitado

--- app/src/main/java/com/jandres/delcassarapp/MainActivity.kt
+++ app/src/main/java/com/jandres/delcassarapp/MainActivity.kt
@@ -2,10 +2,24 @@ package com.jandres.delcassarapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("metodo","onDestroy")
     }
 }
